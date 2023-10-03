bfe626f4bccbef04348daf24ae7f692d50d951e9
8anime.to <philiprigha8@gmail.com>
2023-10-03 10:32:55 +0300

    Add test cases for isValidPassword function

--- tests/test_regexPatterns.py
+++ tests/test_regexPatterns.py
@@ -1,5 +1,15 @@
 
+import pytest
 
+from passwordChecker.regexPatterns import isValidPassword
+
+# Test if isValidPassword correctly identifies a valid password.
+def testValidPassword():
+    assert isValidPassword('Abcdefghi1') == True
+
+# Test if isValidPassword correctly identifies an invalid password.
+def testInvalidPassword():
+    assert isValidPassword('abc123')  == False
 
 
 
